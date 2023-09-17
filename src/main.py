11167d5141a413a3ab19a3e34d7d1d8850edd023
Dylan <inutuzumab@gmail.com>
2023-09-17 12:48:03 +1000

    rebirth commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from astropy.io import fits
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+star_data_image = fits.util.get_testdata_filepath('src/data/star_data.fits')
+star_data = fits.open(star_data_image)
+star_data.info()
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+model.fit(xs, ys, epochs=500)
+print(model.predict([10.0]))
+
